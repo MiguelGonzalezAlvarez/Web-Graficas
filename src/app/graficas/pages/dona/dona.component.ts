ce0f0595b6eaaae61d759c0bba39d14c7681459c
MiguelMuros <miggonzalv@gmail.com>
2021-11-06 18:02:41 +0100

    feat: create barras-dobles screen and dona screen

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Label, MultiDataSet, Color } from 'ng2-charts';
 
 @Component({
   selector: 'app-dona',
   templateUrl: './dona.component.html',
-  styles: [
-  ]
+  styles: []
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
 
-  constructor() { }
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Other'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100, 150]
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
 
-  ngOnInit(): void {
-  }
-
+  public colors: Color[] = [
+    {
+      backgroundColor: [
+        '#0075ED',
+        '#00BAF7',
+        '#00E0DB',
+        '#00F7AD',
+        '#00ED63',
+      ]
+    }
+  ]
 }
