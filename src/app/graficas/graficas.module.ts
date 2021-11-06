0476675c2aa18f0582d7d312b4f583e60f445d26
MiguelMuros <miggonzalv@gmail.com>
2021-11-06 17:10:27 +0100

    feat: install necessary libraries and design barras screen

--- src/app/graficas/graficas.module.ts
+++ src/app/graficas/graficas.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ChartsModule } from 'ng2-charts';
+
 import { GraficasRoutingModule } from './graficas-routing.module';
 import { BarrasComponent } from './pages/barras/barras.component';
 import { BarrasDobleComponent } from './pages/barras-dobles/barras-dobles.component';
@@ -19,7 +21,8 @@ import { DonaComponent } from './pages/dona/dona.component';
   ],
   imports: [
     CommonModule,
-    GraficasRoutingModule
+    GraficasRoutingModule,
+    ChartsModule
   ]
 })
 export class GraficasModule { }
