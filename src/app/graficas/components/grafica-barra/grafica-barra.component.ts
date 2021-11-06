9d9819e4291714c82b6f71a900f7897e7dc5342a
MiguelMuros <miggonzalv@gmail.com>
2021-11-06 17:12:46 +0100

    feat: create grafica-barra component

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
@@ -8,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraficaBarraComponent implements OnInit {
 
-  constructor() { }
+  @Input() horizontal: boolean = false;
+
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+
+  @Input() barChartLabels: Label[] = [];
+
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+
+  @Input() barChartData: ChartDataSets[] = [];
 
   ngOnInit(): void {
+    if (this.horizontal) {
+      this.barChartType = 'horizontalBar';
+    }
   }
 
 }
